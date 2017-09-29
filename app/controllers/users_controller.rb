8d4b2d8603f4a039b25d2be66afc7cb2dbe02f6c
Rio <rnose512@gmail.com>
2017-09-29 08:53:23 -0700

    This change implements the api calls for users in a searched location.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,7 @@
 class UsersController < ApplicationController
 	include SessionsHelper
-	def index
-		if params[:search]
-			users = User.where(location: params[:search]).where(accepting_guests: true) +
-			User.where(location: params[:search]).where(accepting_guests: nil)
-    else
-			users = User.where(location: current_user).where(accepting_guests: true) +
-			User.where(location: current_user).where(accepting_guests: nil)
-    end
 
-    render json: users
+	def index
 	end
 
 	def new
